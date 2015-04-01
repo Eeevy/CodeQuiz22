92b7a4bdd869cc94a0679f3a5fd36c0521c20b9b
Bodestad <gustav.bodestad@gmail.com>
2015-04-01 13:58:51 +0200

    no message

--- src/codequiz/Question.java
+++ src/codequiz/Question.java
@@ -1,11 +1,7 @@
 package codequiz;
   
 public class Question {
-      /**
-       * Andra versionen
-       * GIT <3
-       * 
-       */
+    
     private String question;
     private String answer1;
     private String answer2;
