1bb182a0782c952fd1c3ea5f8f6d4fd6628e063e
Johan Johansson <Johan@Johans-MacBook-Pro-2.local>
2015-04-24 13:11:27 +0200

    no message

---
+++ src/CodeQuizServer/Database.java
@@ -0,0 +1,20 @@
+package CodeQuizServer;
+import java.sql.*;
+
+public class Database {
+
+	public static void main(String [] args) {
+		try{
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/quizquestions", "root", "qweasd123");
+			Statement stat = conn.createStatement();
+			ResultSet rs = stat.executeQuery("select * from Question");
+			
+			while(rs.next()){
+				System.out.println(rs.getString("QuestionID") + rs.getString("Question")+ "\n"
+				+ rs.getString("Answer1"));
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+}
